e927757a8d50bbff03b27aca5cf98b2675abb419
eduarxdogar <cristiangarzon1224@hotmail.com>
2024-11-20 15:56:07 -0600

    V1.0

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -2,13 +2,14 @@ import { Component, signal } from '@angular/core';
 import { CounterComponent } from './../../../shared/components/counter/counter.component';
 import { CommonModule } from '@angular/common';
 import { WaveAudioComponent } from './../../../info/components/wave-audio/wave-audio.component';
+import { HighlinghtDirective } from './../../../shared/directives/highlinght.directive';
 
 
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [CounterComponent, CommonModule, WaveAudioComponent],
+  imports: [CounterComponent, CommonModule, WaveAudioComponent, HighlinghtDirective],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
